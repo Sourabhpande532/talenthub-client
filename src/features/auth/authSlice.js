de20716ec2e1bc77c211bdfdd1014c4ed4f93976
sourabhpande532 <sourabhpande43@gmail.com>
2026-07-30 18:25:00 +0530

    feat: implement login and registration with createAsyncThunk

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -7,11 +7,26 @@ export const registerUser = createAsyncThunk(
   async (userData, { rejectWithValue }) => {
     try {
       const response = await API_URL.post("/auth/register", userData);
-      console.log(response);
+      console.log("register login", response);
       return response.data;
     } catch (error) {
       return rejectWithValue(
         error.response?.data?.message || "Registration failed",
+      );
+    }
+  },
+);
+
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const response = await API_URL.post("/auth/login", credentials);
+      console.log("response login:", response);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(
+        error.response?.data?.message || "Invalid email or password",
       );
     }
   },
@@ -49,8 +64,47 @@ const authSlice = createSlice({
       localStorage.setItem("user", JSON.stringify(action.payload));
     },
   },
+
   extraReducers: (builder) => {
     // Register
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.status = "loading";
+        state.error = null;
+      })
+      .addCase(registerUser.fulfilled, (state) => {
+        state.status = "succeeded";
+        toast.success("Registration successful! Please login.");
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+        toast.error(action.payload);
+      })
+      // Login
+      .addCase(loginUser.pending, (state) => {
+        state.status = "loading";
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.token = action.payload.token;
+        const user = { ...action.payload.user };
+        const tempResume = localStorage.getItem("temp_resume");
+        if (tempResume && user.role === "Applicant") {
+          user.resume = tempResume;
+          localStorage.removeItem("temp_resume");
+        }
+        state.user = user;
+        localStorage.setItem("token", action.payload.token);
+        localStorage.setItem("user", JSON.stringify(user));
+        toast.success("Logged in successfully!");
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+        toast.error(action.payload);
+      });
   },
 });
 
